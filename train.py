33cd16177131d30c2dc82121f660fc5842268a2a
mfon-uwa <55540244+mfon-uwa@users.noreply.github.com>
2021-06-04 19:43:44 +0100

    Add files via upload

--- train.py
+++ train.py
@@ -9,20 +9,7 @@ from sklearn.preprocessing import OneHotEncoder
 import pandas as pd
 from azureml.core.run import Run
 from azureml.data.dataset_factory import TabularDatasetFactory
-
-# TODO: Create TabularDataset using TabularDatasetFactory
-# Data is located at:
-# "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
-
-ds = ### YOUR CODE HERE ###
-
-x, y = clean_data(ds)
-
-# TODO: Split data into train and test sets.
-
-### YOUR CODE HERE ###a
-
-run = Run.get_context()
+from azureml.core import Dataset
 
 def clean_data(data):
     # Dict for cleaning data
@@ -49,7 +36,57 @@ def clean_data(data):
     x_df["poutcome"] = x_df.poutcome.apply(lambda s: 1 if s == "success" else 0)
 
     y_df = x_df.pop("y").apply(lambda s: 1 if s == "yes" else 0)
-    
+
+    return x_df, y_df
+
+
+def auto_clean_data(data):
+    # Dict for cleaning data
+    months = {"jan":1, "feb":2, "mar":3, "apr":4, "may":5, "jun":6, "jul":7, "aug":8, "sep":9, "oct":10, "nov":11, "dec":12}
+    weekdays = {"mon":1, "tue":2, "wed":3, "thu":4, "fri":5, "sat":6, "sun":7}
+
+    # Clean and one hot encode data
+    x_df = data.to_pandas_dataframe().dropna()
+    jobs = pd.get_dummies(x_df.job, prefix="job")
+    x_df.drop("job", inplace=True, axis=1)
+    x_df = x_df.join(jobs)
+    x_df["marital"] = x_df.marital.apply(lambda s: 1 if s == "married" else 0)
+    x_df["default"] = x_df.default.apply(lambda s: 1 if s == "yes" else 0)
+    x_df["housing"] = x_df.housing.apply(lambda s: 1 if s == "yes" else 0)
+    x_df["loan"] = x_df.loan.apply(lambda s: 1 if s == "yes" else 0)
+    contact = pd.get_dummies(x_df.contact, prefix="contact")
+    x_df.drop("contact", inplace=True, axis=1)
+    x_df = x_df.join(contact)
+    education = pd.get_dummies(x_df.education, prefix="education")
+    x_df.drop("education", inplace=True, axis=1)
+    x_df = x_df.join(education)
+    x_df["month"] = x_df.month.map(months)
+    x_df["day_of_week"] = x_df.day_of_week.map(weekdays)
+    x_df["poutcome"] = x_df.poutcome.apply(lambda s: 1 if s == "success" else 0)
+
+    x_df["y"].apply(lambda s: 1 if s == "yes" else 0)
+
+    return x_df
+
+
+# TODO: Create TabularDataset using TabularDatasetFactory
+# Data is located at:
+# "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
+
+### YOUR CODE HERE ###
+ds_path = "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
+
+ds = Dataset.Tabular.from_delimited_files(path=ds_path) 
+
+x, y = clean_data(ds)
+
+# TODO: Split data into train and test sets.
+### YOUR CODE HERE ###a
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=11)
+
+
+run = Run.get_context()
+
 
 def main():
     # Add arguments to script
@@ -70,3 +107,5 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
